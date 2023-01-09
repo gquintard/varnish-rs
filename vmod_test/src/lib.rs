656095a83931807f16532407567df662cc783ec4
Guillaume Quintard <guillaume.quintard@gmail.com>
2023-01-08 19:38:43 -0800

    vmod_test needed to know about vcl::Error

--- vmod_test/src/lib.rs
+++ vmod_test/src/lib.rs
@@ -5,6 +5,7 @@ use std::time::Duration;
 
 use varnish::vcl::ctx::Ctx;
 use varnish::vcl::probe;
+use varnish::vcl::Result;
 
 varnish::vtc!(test01);
 varnish::vtc!(test02);
@@ -12,27 +13,24 @@ varnish::vtc!(test03);
 varnish::vtc!(test04);
 varnish::vtc!(test05);
 
-pub fn set_hdr(ctx: &mut Ctx, name: &str, value: &str) -> Result<(), String> {
+pub fn set_hdr(ctx: &mut Ctx, name: &str, value: &str) -> Result<()> {
     if let Some(ref mut req) = ctx.http_req {
         req.set_header(name, value)
     } else {
-        Err("http_req isn't accessible".to_string())
+        Err("http_req isn't accessible".into())
     }
 }
 
-pub fn unset_hdr(ctx: &mut Ctx, name: &str) -> Result<(), String> {
+pub fn unset_hdr(ctx: &mut Ctx, name: &str) -> Result<()> {
     if let Some(ref mut req) = ctx.http_req {
         req.unset_header(name);
         Ok(())
     } else {
-        Err("http_req isn't accessible".to_string())
+        Err("http_req isn't accessible".into())
     }
 }
 
-pub fn ws_reserve<'a, 'b>(
-    ctx: &'b mut Ctx<'a>,
-    s: &str,
-) -> Result<varnish_sys::VCL_STRING, String> {
+pub fn ws_reserve<'a, 'b>(ctx: &'b mut Ctx<'a>, s: &str) -> Result<varnish_sys::VCL_STRING> {
     let mut rbuf = ctx.ws.reserve();
     match write!(rbuf.buf, "{} {} {}\0", s, s, s) {
         Ok(()) => {
@@ -40,7 +38,7 @@ pub fn ws_reserve<'a, 'b>(
             assert_eq!(final_buf.len(), 3 * s.len() + 3);
             Ok(final_buf.as_ptr() as *const i8)
         }
-        _ => Err("workspace issue".to_owned()),
+        _ => Err("workspace issue".into()),
     }
 }
 
@@ -48,7 +46,7 @@ pub fn out_str(_: &mut Ctx) -> &'static str {
     "str"
 }
 
-pub fn out_res_str(_: &mut Ctx) -> Result<&'static str, String> {
+pub fn out_res_str(_: &mut Ctx) -> Result<&'static str> {
     Ok("str")
 }
 
@@ -56,7 +54,7 @@ pub fn out_string(_: &mut Ctx) -> String {
     "str".to_owned()
 }
 
-pub fn out_res_string(_: &mut Ctx) -> Result<String, String> {
+pub fn out_res_string(_: &mut Ctx) -> Result<String> {
     Ok("str".to_owned())
 }
 
@@ -64,7 +62,7 @@ pub fn out_bool(_: &mut Ctx) -> bool {
     true
 }
 
-pub fn out_res_bool(_: &mut Ctx) -> Result<bool, String> {
+pub fn out_res_bool(_: &mut Ctx) -> Result<bool> {
     Ok(true)
 }
 
@@ -72,13 +70,13 @@ pub fn out_duration(_: &mut Ctx) -> Duration {
     Duration::new(0, 0)
 }
 
-pub fn out_res_duration(_: &mut Ctx) -> Result<Duration, String> {
+pub fn out_res_duration(_: &mut Ctx) -> Result<Duration> {
     Ok(Duration::new(0, 0))
 }
 
 // this is a pretty terrible idea, the request body is probably big, and your workspace is tiny,
 // but hey, it's a test function
-pub fn req_body(ctx: &mut Ctx) -> Result<varnish_sys::VCL_STRING, String> {
+pub fn req_body(ctx: &mut Ctx) -> Result<varnish_sys::VCL_STRING> {
     let mut body_chunks = ctx.cached_req_body()?;
     // make sure the body will be null-terminated
     body_chunks.push(b"\0");
