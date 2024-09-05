e1909b5ee45026750a1a8495293ed917730cf12e
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2024-09-04 23:52:22 -0400

    Consolidate `Error` and `Result`
    There does not seem to be any reason to keep `Error` and `Result` in both `vcl` and `vsc` modules.
    
    In the future, I think it would be better to rename them, e.g. to `VarnishError` and `VarnishResult`.  This is a common practice now to distinguish them from the built-in ones (lots of people have been complaining that `std::io::Result` and others didn't follow that convention, but it is too late for that now).

---
+++ src/error.rs
@@ -0,0 +1,43 @@
+/// custom vcl `Error` type
+///
+/// The C errors aren't typed and are just C strings, so we just wrap them into a proper rust
+/// `Error`
+pub struct Error {
+    s: String,
+}
+
+impl Error {
+    /// Create a new `Error` from a string
+    pub(crate) fn new(s: String) -> Self {
+        Error { s }
+    }
+}
+
+impl std::fmt::Debug for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        std::fmt::Debug::fmt(&self.s, f)
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        std::fmt::Display::fmt(&self.s, f)
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl From<String> for Error {
+    fn from(s: String) -> Self {
+        Error { s }
+    }
+}
+
+impl From<&str> for Error {
+    fn from(s: &str) -> Self {
+        Error { s: s.into() }
+    }
+}
+
+/// Shorthand to `std::result::Result<T, Error>`
+pub type Result<T> = std::result::Result<T, Error>;
