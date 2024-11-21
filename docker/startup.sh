33d2c8fe247b1babb943f4acf1f3d3916505470c
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2024-11-21 14:07:25 -0500

    add multi-version docker support
    Docker allows multiple Varnish versions on the same machine.  To use it, run `just docker-run-76` or any other version (run `just` to see a list).  On the first run, it will build the image, install needed dependencies, and then it will start the container.  The container will have the git root directory mounted as `/app`, so you can run `cargo build` and `cargo test` as usual. All work is preserved in the `docker/.cache/<version>`, so restarting container would not need re-install everything.

---
+++ docker/startup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ ! -d /app/.github ] || [ ! -d ~/.cache ]; then
+    echo " "
+    echo "ERROR: Docker container was not started properly."
+    echo "       Use   just docker-run-76  or another version."
+    exit 1
+fi
+
+export PATH="$PATH:~/.local/bin/"
+
+export CARGO_TARGET_DIR=~/.cache/target
+if [ ! -f "$CARGO_HOME/env" ]; then
+    echo "Downloading and installing Rust..."
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+fi
+. "$CARGO_HOME/env"
+
+if ! command -v cargo-binstall; then
+  echo "cargo-binstall is not found. Installing..." >&2
+  curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+fi
+
+cargo binstall --no-confirm just ripgrep
+
+exec "$@"
