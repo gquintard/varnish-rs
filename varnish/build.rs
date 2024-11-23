6348a77a29ee6065bef798932cfd052f2ff7e240
Guillaume Quintard <guillaume.quintard@varnish-software.com>
2024-11-22 21:51:47 -0800

    build.rs

---
+++ varnish/build.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("cargo::rustc-check-cfg=cfg(lts_60)");
+    if let Ok(v) = std::env::var("DEP_VARNISHAPI_VERSION_NUMBER") {
+        if v.starts_with("6.0.") {
+            println!("cargo::rustc-cfg=lts_60");
+        println!("cargo::warning=------------{v}");
+        }
+    }
+}
